cd251a5cf41cc8c37dfa070665a29bc3bd067c97
Adrian Suter <adrian@suter-wirz.ch>
2019-05-11 22:48:52 +0200

    Update copyright year

--- src/happynumbers/Main.java
+++ src/happynumbers/Main.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2016 Adrian Suter, https://github.com/adriansuter/.
+ * Copyright 2016-2019 Adrian Suter, https://github.com/adriansuter/.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
